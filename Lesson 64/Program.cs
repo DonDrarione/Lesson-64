bf450ec62034529af1075b35d77c048d190fe69f
sunny <sunny@DESKTOP-UJ1BUSP>
2023-06-06 21:36:49 +0300

    Добавьте файлы проекта.

---
+++ Lesson 64/Program.cs
@@ -0,0 +1,24 @@
+﻿Writer(N());
+
+int N()
+{
+    Console.WriteLine("введите N");
+    var N = Convert.ToInt32(Console.ReadLine());
+    while (N < 1)
+    {
+        Console.WriteLine("введено неправильное значение");
+        N = Convert.ToInt32(Console.ReadLine());
+    }
+    return N;
+}
+
+
+void Writer (int N)
+{
+    if (N > 0)
+    {
+        Console.Write(N + (N>1 ? ",":""));
+        Writer(N - 1);
+    }
+
+}
